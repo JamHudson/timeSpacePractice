99175d2eaca211c7254fa65ff10fddab77f95a17
SamuelM007 <174657033+SamuelM007@users.noreply.github.com>
2026-04-21 12:40:18 -0700

    test1

--- src/PracticeTest.java
+++ src/PracticeTest.java
@@ -1,8 +1,17 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-
 public class PracticeTest {
+
+  @Test
+  void testMostCommonTimeEfficientWithNullArray(){
+    int[] array = {};
+
+    int actual = Practice.mostCommonTimeEfficient(array);
+
+    assertEquals(0,actual);
+  }
+
   
   // TODO: Implement tests for Practice.mostCommonTimeEfficient and Practice.mostCommonSpaceEfficient
 
